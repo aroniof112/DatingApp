58304f035f2625d703360413b06aa3b3e169a0cb
Anto <forgaci.antonio@gmail.com>
2023-02-01 00:33:46 +0200

    Initial comit end of cap 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(DataContext context, ILogger<UsersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync(); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
